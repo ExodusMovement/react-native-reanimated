558f61f2f71402ae3b2ed3a8de478a6ea69326d3
Mark Vayngrib <mvayngrib@gmail.com>
2024-07-04 17:21:07 -0400

    feat: support enabling web via a flag in dev mode

--- src/reanimated2/PlatformChecker.ts
+++ src/reanimated2/PlatformChecker.ts
@@ -13,7 +13,7 @@ export function isWeb(): boolean {
 }
 
 export function shouldBeUseWeb() {
-  return isJest() || isChromeDebugger() || isWeb();
+  return isJest() || isChromeDebugger() || isWeb() || (global.__DEV__ && process.env.REANIMATED_WEB === 'true');
 }
 
 export function nativeShouldBeMock() {

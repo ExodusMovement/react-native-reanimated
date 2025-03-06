e0ddaaf4d8cd895973755a66d02b0d905512eb3e
Mark Vayngrib <mvayngrib@gmail.com>
2025-03-06 15:32:32 -0500

    chore: rm reanimated eslint plugin

--- packages/eslint-plugin-reanimated/src/index.ts
+++
@@ -1,9 +0,0 @@
-import type { TSESLint } from '@typescript-eslint/utils';
-import noAnimatedStyleToNonAnimatedComponent from './noAnimatedStyleToNonAnimatedComponent';
-import useReanimatedError from './useReanimatedError';
-
-export const rules = {
-  'animated-style-non-animated-component':
-    noAnimatedStyleToNonAnimatedComponent,
-  'use-reanimated-error': useReanimatedError,
-} satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>;

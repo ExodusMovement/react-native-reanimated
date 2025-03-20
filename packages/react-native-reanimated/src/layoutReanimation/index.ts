ee256f8b9e6d8b6473d3c5a41eaee60b279df88b
Joshua Rogers <joshua.ro@exodus.io>
2025-03-20 19:46:57 +1100

    security: Attempt to disable keyframes

--- packages/react-native-reanimated/src/layoutReanimation/index.ts
+++ packages/react-native-reanimated/src/layoutReanimation/index.ts
@@ -4,4 +4,4 @@ export * from './animationBuilder';
 export * from './defaultAnimations';
 export * from './defaultTransitions';
 export * from './sharedTransitions';
-export type { KeyframeProps } from './animationBuilder/commonTypes';
+/* export type { KeyframeProps } from './animationBuilder/commonTypes'; */ // disabled, contact appsec if needed: https://github.com/ExodusMovement/exodus-mobile/pull/24699#issuecomment-2709694172

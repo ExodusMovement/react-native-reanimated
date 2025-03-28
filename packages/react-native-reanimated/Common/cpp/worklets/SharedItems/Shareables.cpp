a21758b80b8047bf471074516f90e74797a6a93c
kewde <kewde@particl.io>
2025-03-28 23:21:19 +0100

    fix: poc remove eval

--- packages/react-native-reanimated/Common/cpp/worklets/SharedItems/Shareables.cpp
+++ packages/react-native-reanimated/Common/cpp/worklets/SharedItems/Shareables.cpp
@@ -276,9 +276,27 @@ jsi::Value ShareableWorklet::toJSValue(jsi::Runtime &rt) {
           data_.cend(),
           [](const auto &item) { return item.first == "__workletHash"; }) &&
       "ShareableWorklet doesn't have `__workletHash` property");
+  // Copy the code & sourceURL from the worklet object to ensure
+  // that the code is not modified in the original object.
   jsi::Value obj = ShareableObject::toJSValue(rt);
+  auto initData = obj.asObject(rt).getProperty(rt, "__initData").asObject(rt);
+  auto code = std::make_shared<const jsi::StringBuffer>(
+      initData.getProperty(rt, "code").asString(rt).utf8(rt));
+  auto sourceURL = initData.getProperty(rt, "location").asString(rt).utf8(rt);
+  // The code has to be evaluated in the context of the worklet runtime.
+  // This is done by creating a new function that evaluates the code and
+  // returns the resulting function.
+  auto evaluateWorkletFunction = jsi::Function::createFromHostFunction(
+      rt,
+      jsi::PropNameID::forAscii(rt, "compileWorklet"),
+      0,
+      [&](jsi::Runtime &rt, const jsi::Value &, const jsi::Value *, size_t)
+          -> jsi::Value { return rt.evaluateJavaScript(code, sourceURL); });
   return getValueUnpacker(rt).call(
-      rt, obj, jsi::String::createFromAscii(rt, "Worklet"));
+      rt,
+      obj,
+      jsi::String::createFromAscii(rt, "Worklet"),
+      evaluateWorkletFunction);
 }
 
 jsi::Value ShareableRemoteFunction::toJSValue(jsi::Runtime &rt) {
---
+++ packages/react-native-reanimated/src/valueUnpacker.ts
@@ -0,0 +1,85 @@
+/* eslint-disable reanimated/use-reanimated-error */
+'use strict';
+import { shouldBeUseWeb } from './PlatformChecker';
+import { isWorkletFunction } from './commonTypes';
+import type { WorkletFunction } from './commonTypes';
+
+function valueUnpacker(
+  objectToUnpack: any,
+  category?: string,
+  remoteFunctionName?: string,
+  evaluateWorkletFunction?: () => Function
+): any {
+  'worklet';
+  let workletsCache = global.__workletsCache;
+  let handleCache = global.__handleCache;
+  if (workletsCache === undefined) {
+    // init
+    workletsCache = global.__workletsCache = new Map();
+    handleCache = global.__handleCache = new WeakMap();
+  }
+  const workletHash = objectToUnpack.__workletHash;
+  if (workletHash !== undefined) {
+    let workletFun = workletsCache.get(workletHash);
+    if (workletFun === undefined) {
+      workletFun = evaluateWorkletFunction?.();
+      workletsCache.set(workletHash, workletFun);
+    }
+    const functionInstance = workletFun.bind(objectToUnpack);
+    objectToUnpack._recur = functionInstance;
+    return functionInstance;
+  } else if (objectToUnpack.__init !== undefined) {
+    let value = handleCache.get(objectToUnpack);
+    if (value === undefined) {
+      value = objectToUnpack.__init();
+      handleCache.set(objectToUnpack, value);
+    }
+    return value;
+  } else if (category === 'RemoteFunction') {
+    const fun = () => {
+      const label = remoteFunctionName
+        ? `function \`${remoteFunctionName}\``
+        : 'anonymous function';
+      throw new Error(`[Reanimated] Tried to synchronously call a non-worklet ${label} on the UI thread.
+See https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshooting#tried-to-synchronously-call-a-non-worklet-function-on-the-ui-thread for more details.`);
+    };
+    fun.__remoteFunction = objectToUnpack;
+    return fun;
+  } else {
+    throw new Error(
+      `[Reanimated] Data type in category "${category}" not recognized by value unpacker: "${_toString(
+        objectToUnpack
+      )}".`
+    );
+  }
+}
+
+type ValueUnpacker = WorkletFunction<
+  [objectToUnpack: any, category?: string],
+  any
+>;
+
+if (__DEV__ && !shouldBeUseWeb()) {
+  const testWorklet = (() => {
+    'worklet';
+  }) as WorkletFunction<[], void>;
+  if (!isWorkletFunction(testWorklet)) {
+    throw new Error(
+      `[Reanimated] Failed to create a worklet. See https://docs.swmansion.com/react-native-reanimated/docs/guides/troubleshooting#failed-to-create-a-worklet for more details.`
+    );
+  }
+  if (!isWorkletFunction(valueUnpacker)) {
+    throw new Error('[Reanimated] `valueUnpacker` is not a worklet');
+  }
+  const closure = (valueUnpacker as ValueUnpacker).__closure;
+  if (closure === undefined) {
+    throw new Error('[Reanimated] `valueUnpacker` closure is undefined');
+  }
+  if (Object.keys(closure).length !== 0) {
+    throw new Error('[Reanimated] `valueUnpacker` must have empty closure');
+  }
+}
+
+export function getValueUnpackerCode() {
+  return (valueUnpacker as ValueUnpacker).__initData.code;
+}

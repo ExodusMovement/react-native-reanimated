2bfbf58f64098310f1efd5e1a3e4f32fc42e12a9
alexandrius <s.pataridze@gmail.com>
2025-04-15 11:30:21 +0400

    refactor: revert screens patch

--- packages/react-native-reanimated/apple/reanimated/apple/LayoutReanimation/REAScreensHelper.h
+++ packages/react-native-reanimated/apple/reanimated/apple/LayoutReanimation/REAScreensHelper.h
@@ -1,4 +1,6 @@
-#define LOAD_SCREENS_HEADERS false
+#define LOAD_SCREENS_HEADERS                                         \
+  ((!RCT_NEW_ARCH_ENABLED && __has_include(<RNScreens/RNSScreen.h>)) \
+  || (RCT_NEW_ARCH_ENABLED && __has_include(<RNScreens/RNSScreen.h>) && __cplusplus))
 
 #if LOAD_SCREENS_HEADERS
 #import <RNScreens/RNSEnums.h>
